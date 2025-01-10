a7500cc9090a398649f74bd30e4b6a87426fcae5
AltMapper <maksimka.volkov.2008@list.ru>
2025-01-11 04:04:34 +0700

    Логотип, футболки, немного наших YML

--- Content.Shared/Backmen/CCVar/CCVars.cs
+++ Content.Shared/Backmen/CCVar/CCVars.cs
@@ -262,13 +262,13 @@ public sealed class CCVars
     #region Mood System
 
     public static readonly CVarDef<bool> MoodEnabled =
-        CVarDef.Create("mood.enabled", true, CVar.SERVER);
+        CVarDef.Create("mood.enabled", false, CVar.SERVER);
 
     public static readonly CVarDef<bool> MoodIncreasesSpeed =
-        CVarDef.Create("mood.increases_speed", true, CVar.SERVER);
+        CVarDef.Create("mood.increases_speed", false, CVar.SERVER);
 
     public static readonly CVarDef<bool> MoodDecreasesSpeed =
-        CVarDef.Create("mood.decreases_speed", true, CVar.SERVER);
+        CVarDef.Create("mood.decreases_speed", false, CVar.SERVER);
 
     public static readonly CVarDef<bool> MoodModifiesThresholds =
         CVarDef.Create("mood.modify_thresholds", false, CVar.SERVER);

b42f28768063450992c4bbd9f5b5c983a1f8d6c5
CrimeMoot <wakeafa@gmail.com>
2025-06-22 21:10:07 +0500

    passed

--- Tools/ganimed/locale_check_ftl.py
+++ Tools/ganimed/locale_check_ftl.py
@@ -41,7 +41,6 @@ def read_ftl_file(path):
             if key in keys:
                 duplicates.add(key)
             keys[key] = val
-            print(f"Loaded key '{key}' with value start: '{val[:30]}'")
         else:
             i += 1
 

b658c2bb05711074bc30a3e99694f97b1c90f02e
CrimeMoot <wakeafa@gmail.com>
2025-06-27 15:04:13 +0500

    trash

--- Content.Client/Corvax/Sponsors/SponsorsManager.cs
+++ Content.Client/Corvax/Sponsors/SponsorsManager.cs
@@ -2,31 +2,30 @@
 using Content.Shared.Corvax.Sponsors;
 using Robust.Shared.Network;
 
-namespace Content.Client.Corvax.Sponsors
+namespace Content.Client.Corvax.Sponsors;
+
+public sealed class SponsorsManager : ISponsorsManager // Ganimed-Sponsors
 {
-    public sealed class SponsorsManager : ISponsorsManager // Ganimed-Sponsors
+    [Dependency] private readonly IClientNetManager _netMgr = default!;
+
+    private SponsorInfo? _info;
+
+    public void Initialize()
     {
-        [Dependency] private readonly IClientNetManager _netMgr = default!;
-
-        private SponsorInfo? _info;
-
-        public void Initialize()
-        {
-            _netMgr.RegisterNetMessage<MsgSponsorInfo>(msg => _info = msg.Info);
-        }
-
-        public bool TryGetInfo([NotNullWhen(true)] out SponsorInfo? sponsor)
-        {
-            sponsor = _info;
-            return _info != null;
-        }
-
-        // Ganimed-Sponsors start
-        bool ISponsorsManager.TryGetInfo(Robust.Shared.Network.NetUserId userId, [NotNullWhen(true)] out SponsorInfo? sponsor)
-        {
-            sponsor = null;
-            return false;
-        }
-        // Ganimed-Sponsors end
+        _netMgr.RegisterNetMessage<MsgSponsorInfo>(msg => _info = msg.Info);
     }
+
+    public bool TryGetInfo([NotNullWhen(true)] out SponsorInfo? sponsor)
+    {
+        sponsor = _info;
+        return _info != null;
+    }
+
+    // Ganimed-Sponsors start
+    bool ISponsorsManager.TryGetInfo(Robust.Shared.Network.NetUserId userId, [NotNullWhen(true)] out SponsorInfo? sponsor)
+    {
+         sponsor = null;
+        return false;
+    }
+    // Ganimed-Sponsors end
 }

c61494e3df86d3fd70e3ab0a1a7d3ec0d73a611c
CrimeMoot <wakeafa@gmail.com>
2025-07-21 16:42:45 +0500

    oops

--- Content.Client/Alerts/ClientAlertsSystem.cs
+++ Content.Client/Alerts/ClientAlertsSystem.cs
@@ -91,6 +91,11 @@ public sealed class ClientAlertsSystem : AlertsSystem
         UpdateHud(alerts);
     }
 
+    private void ClientAlertsHandleState(Entity<AlertsComponent> alerts, ref AfterAutoHandleStateEvent args)
+    {
+        UpdateHud(alerts);
+    }
+
     private void UpdateHud(Entity<AlertsComponent> entity)
     {
         if (_playerManager.LocalEntity == entity.Owner)

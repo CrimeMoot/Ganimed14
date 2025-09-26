b5c4f412cd98e84890e4fdf080a33982013d4e6a
CrimeMoot <wakeafa@gmail.com>
2025-09-26 19:41:36 +0500

    ObjectiveSecCount

---
+++ Content.Server/_Ganimed/Objectives/Components/ObjectiveSecCountComponent.cs
@@ -0,0 +1,10 @@
+using Content.Server.Objectives.Systems;
+
+namespace Content.Server._Ganimed.Objectives.Components;
+
+[RegisterComponent]
+public sealed partial class ObjectiveSecCountComponent : Component
+{
+    [DataField]
+    public int MinSec = 2;
+}
